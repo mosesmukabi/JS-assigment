85ed6b32d146e38de1d381abc5507898d3c35f61
mosesmukabi <mosesmukabi9@gmail.com>
2024-09-14 11:43:29 -0700

    js addition, product, increment and conversion

---
+++ main.js
@@ -0,0 +1,25 @@
+// area of a triage
+const baseValue = 3;
+const heightValue = 6;
+
+const areaValue = (baseValue * heightValue) / 2;
+console.log(`the area is ${areaValue}`);
+// sum of two numbers
+let x = 5;
+let y = 3;
+
+const sum = x + y;
+console.log(`the sum is ${sum}`);
+// conversion into minutes
+let timeInMinutes = 5;
+const timeInSeconds = timeInMinutes * 60;
+
+console.log(`the time in seconds is ${timeInSeconds}`);
+// incrementing a number
+
+function addition(number) {
+    return number + 1;
+  }
+
+console.log(addition(7));
+
