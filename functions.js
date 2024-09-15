37cca4eed98d423f6c4353806ebb09e1d3660993
moses mukabi <mosesmukabi9@gmai.com>
2024-09-15 00:20:43 -0700

    used fuctions in every case

---
+++ functions.js
@@ -0,0 +1,30 @@
+// sum of two numbers fuction
+function addation(number1, number2){
+  return number1 + number2
+}
+console.log(addation(6,9));
+console.log(addation(7,9));
+
+// conversion into minutes
+function conversion(minutes){
+  return minutes * 60
+}
+console.log(conversion(6));
+console.log(conversion(9));
+
+// incrementing a number
+
+function increase(number) {
+    return number + 1;
+  }
+
+console.log(increase(7));
+console.log(increase(67));
+
+// area of a triangle
+function areaTriangle(base, height){
+  return (base * height) / 2
+}
+console.log(areaTriangle(6,6));
+console.log(areaTriangle(10,8));
+
